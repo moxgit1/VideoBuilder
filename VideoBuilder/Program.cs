3eb10ecb7785da0e9483b9b9393c4317f12df6f7
sanjay <raaosy@yahoo.com>
2020-09-13 11:42:24 -0700

    Builder Pattern sample code
    Builder Pattern sample code for test

---
+++ VideoBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VideoBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
